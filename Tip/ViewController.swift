51eaf312d5a77a6515d52521edebf050d3237d71
michelle <mimikeoy24@gmail.com>
2020-08-31 05:43:01 +0800

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Michelle on 31/8/2020.
+//  Copyright © 2020 michellekeoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
